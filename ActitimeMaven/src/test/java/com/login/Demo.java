ca87cda6003d150f292bd9a3866f20a8a20dce81
Manoj <Manoj@LAPTOP-U19AQ092>
2020-07-04 10:09:18 +0530

    Project Commit

---
+++ ActitimeMaven/src/test/java/com/login/Demo.java
@@ -0,0 +1,13 @@
+package com.login;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo {
+
+	@Test
+	public void runTest()
+	{
+		Reporter.log("test running successfully",true);
+	}
+}
